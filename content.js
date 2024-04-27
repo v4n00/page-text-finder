1fd281df2b6c4d13555da15eacdbbca38b78005d
v4n00 <105587619+v4n00@users.noreply.github.com>
2024-04-27 12:34:37 +0300

    implemented multiple searches functionality

--- content.js
+++ content.js
@@ -40,12 +40,18 @@ document.addEventListener('mouseup', async () => {
 const showMessageOnPage = (displayedText, selectedText) => {
 	clearExistingMessage();
 
-	const innerDiv = document.createElement('div');
-	innerDiv.innerHTML = displayedText;
-	innerDiv.id = 'extensionMessageDiv';
-	innerDiv.innerHTML = innerDiv.innerHTML.replace(selectedText, `<span style="background-color: yellow; color: black;">${selectedText}</span>`);
-	Object.assign(innerDiv.style, {
+	const outerDiv = document.createElement('div');
+	outerDiv.addEventListener('mouseup', (e) => {
+		e.stopPropagation();
+	});
+	outerDiv.id = 'extensionMessageDiv';
+	Object.assign(outerDiv.style, {
 		position: 'fixed',
+		display: 'flex',
+		'flex-direction': 'column',
+		gap: '10px',
+		'justify-content': 'center',
+		'align-items': 'center',
 		bottom: '20px',
 		right: '20px',
 		padding: '10px',
@@ -55,11 +61,62 @@ const showMessageOnPage = (displayedText, selectedText) => {
 		border: '1px solid black',
 		'max-width': '50%',
 		'max-height': '75%',
+		borderRadius: '5px',
+	});
+
+	const optionsDiv = document.createElement('div');
+	Object.assign(optionsDiv.style, {
+		display: 'flex',
+		'justify-content': 'space-around',
+		'align-items': 'center',
+		width: '100%',
+	});
+
+	const matchesText = document.createElement('span');
+	matchesText.innerHTML = `Matches: ${currentFoundIndex + 1}/${foundIndexes.length}`;
+	Object.assign(matchesText.style, {
+		color: 'red',
+		fontWeight: 'bold',
+		'font-size': '1.2em',
+	});
+
+	const prevButton = document.createElement('button');
+	prevButton.innerHTML = 'Prev';
+	prevButton.onmouseup = () => {
+		if (currentFoundIndex > 0) {
+			currentFoundIndex--;
+			showNewMessage(selectedText);
+		}
+	};
+
+	const nextButton = document.createElement('button');
+	nextButton.innerHTML = 'Next';
+	nextButton.onmouseup = () => {
+		if (currentFoundIndex < foundIndexes.length - 1) {
+			currentFoundIndex++;
+			showNewMessage(selectedText);
+		}
+	};
+
+	const innerDiv = document.createElement('div');
+	innerDiv.innerHTML = displayedText;
+	innerDiv.innerHTML = innerDiv.innerHTML.replace(selectedText, `<span style="background-color: yellow; color: black;">${selectedText}</span>`);
+	Object.assign(innerDiv.style, {
+		backgroundColor: 'white',
+		color: 'black',
+		border: '1px solid black',
+		width: '100%',
+		height: '100%',
 		'overflow-y': 'auto',
 		borderRadius: '5px',
 	});
 
-	document.body.appendChild(innerDiv);
+	optionsDiv.appendChild(prevButton);
+	optionsDiv.appendChild(matchesText);
+	optionsDiv.appendChild(nextButton);
+	outerDiv.appendChild(innerDiv);
+	outerDiv.appendChild(optionsDiv);
+	document.body.appendChild(outerDiv);
 };
 
 // utils
@@ -86,4 +143,12 @@ const findAllInstances = (source, target) => {
 const resetFoundIndexes = () => {
 	foundIndexes = [];
 	currentFoundIndex = -1;
+};
+
+const showNewMessage = (selectedText) => {
+	chrome.storage.local.get(['textStorage', 'bufferSize'], (data) => {
+		displayedText = data.textStorage.substring(foundIndexes[currentFoundIndex] - 1000, foundIndexes[currentFoundIndex] + parseInt(data.bufferSize));
+
+		showMessageOnPage(displayedText, selectedText);
+	});
 };
