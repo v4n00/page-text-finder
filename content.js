fc34562ca53be264c5d0ae86877ce8cfff18c392
v4n00 <105587619+v4n00@users.noreply.github.com>
2024-04-27 11:41:38 +0300

    rewrote code to support multiple searches

--- content.js
+++ content.js
@@ -1,43 +1,50 @@
-let toggleState = false;
+let isExtensionOn = false;
+let foundIndexes = [];
+let currentFoundIndex = -1;
 
 chrome.storage.local.get(['toggle'], (result) => {
-	toggleState = result.toggle || false;
+	isExtensionOn = result.toggle || false;
 });
 
 chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
 	if (request.type === 'toggleChanged') {
-		toggleState = request.toggleState;
-		chrome.storage.local.set({ toggle: toggleState });
+		isExtensionOn = request.toggleState;
+		chrome.storage.local.set({ toggle: isExtensionOn });
 		sendResponse({ status: 'Toggle state updated' });
 	}
 });
 
-document.addEventListener('mouseup', () => {
+document.addEventListener('mouseup', async () => {
 	clearExistingMessage();
 
-	if (toggleState) {
+	if (isExtensionOn) {
+		await new Promise((resolve) => setTimeout(resolve, 0));
 		let selectedText = window.getSelection().toString().trim();
 
-		// send the selected text
 		if (selectedText.length > 0) {
-			chrome.runtime.sendMessage({ type: 'TEXT_SELECTED', text: selectedText }, ({ text, index }) => {
-				const div = document.createElement('div');
+			chrome.storage.local.get(['textStorage', 'bufferSize'], (data) => {
+				foundIndexes = findAllInstances(data.textStorage, selectedText);
+				let displayedText = 'not found';
 
-				// if index is found, display the text
-				div.innerHTML = index !== -1 ? text : 'not found';
-				showMessageOnPage(div, selectedText);
+				if (foundIndexes.length > 0) {
+					currentFoundIndex = 0;
+					displayedText = data.textStorage.substring(foundIndexes[currentFoundIndex] - 1000, foundIndexes[currentFoundIndex] + parseInt(data.bufferSize));
+				} else resetFoundIndexes();
+
+				showMessageOnPage(displayedText, selectedText);
 			});
-		}
+		} else resetFoundIndexes();
 	}
 });
 
-const showMessageOnPage = (rawDiv, selectedText) => {
+const showMessageOnPage = (displayedText, selectedText) => {
 	clearExistingMessage();
 
-	// add id and styling to the div
-	rawDiv.id = 'extensionMessageDiv';
-	rawDiv.innerHTML = rawDiv.innerHTML.replace(selectedText, `<span style="background-color: yellow; color: black;">${selectedText}</span>`);
-	Object.assign(rawDiv.style, {
+	const innerDiv = document.createElement('div');
+	innerDiv.innerHTML = displayedText;
+	innerDiv.id = 'extensionMessageDiv';
+	innerDiv.innerHTML = innerDiv.innerHTML.replace(selectedText, `<span style="background-color: yellow; color: black;">${selectedText}</span>`);
+	Object.assign(innerDiv.style, {
 		position: 'fixed',
 		bottom: '20px',
 		right: '20px',
@@ -46,19 +53,37 @@ const showMessageOnPage = (rawDiv, selectedText) => {
 		backgroundColor: 'white',
 		color: 'black',
 		border: '1px solid black',
-		'max-width': '800px',
-		'max-height': '800px',
+		'max-width': '50%',
+		'max-height': '75%',
 		'overflow-y': 'auto',
 		borderRadius: '5px',
 	});
 
-	// set message text and show it
-	document.body.appendChild(rawDiv);
+	document.body.appendChild(innerDiv);
 };
+
+// utils
 
 const clearExistingMessage = () => {
 	let existingMessage = document.getElementById('extensionMessageDiv');
 	if (existingMessage) {
 		existingMessage.parentNode.removeChild(existingMessage);
 	}
+};
+
+const findAllInstances = (source, target) => {
+	let indexes = [];
+	let i = source.indexOf(target);
+
+	while (i !== -1) {
+		indexes.push(i);
+		i = source.indexOf(target, i + 1);
+	}
+
+	return indexes;
+};
+
+const resetFoundIndexes = () => {
+	foundIndexes = [];
+	currentFoundIndex = -1;
 };
